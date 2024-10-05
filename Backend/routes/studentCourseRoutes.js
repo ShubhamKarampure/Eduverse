1c31cb18fe0d03c772ee3f9bfd9af83d76dd7012
Vedant Shah <vedantshah5445@gmail.com>
2024-10-05 18:23:52 +0530

    evaluation added

--- Backend/routes/studentCourseRoutes.js
+++ Backend/routes/studentCourseRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { enrollStudentController, getAllCoursesByBranchController} from '../controllers/courseControllers.js'
-
+import { evaluationController } from '../controllers/testControllers.js'
 const router=express.Router()
 
 
 router.route('/:id').post(enrollStudentController).get(getAllCoursesByBranchController)
+
+router.route('/quiz').post(evaluationController)
 
 export const studentCourseRouter = router
