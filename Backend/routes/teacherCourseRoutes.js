58bca24f0334270d1de6d6c01e4b44848a106435
shubham <shubhamkarampure26@gmail.com>
2024-10-05 18:40:02 +0530

    added course/profile image

--- Backend/routes/teacherCourseRoutes.js
+++ Backend/routes/teacherCourseRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { createCourseController,updateCourseController,deleteCourseController, generateQuizController, getAllCoursesByInstructor} from '../controllers/courseControllers.js'
+import fileUpload from 'express-fileupload'
 
 const router=express.Router()
 
 router.route('/').post(createCourseController).get(getAllCoursesByInstructor)
-router.route('/:id').patch(updateCourseController).delete(deleteCourseController).get(generateQuizController)
+router.route('/:id').patch(fileUpload({
+    useTempFiles: true,
+    tempFileDir: 'C:/Windows/Temp'
+}),updateCourseController).delete(deleteCourseController).get(generateQuizController)
 
 export const teacherCourseRouter = router
