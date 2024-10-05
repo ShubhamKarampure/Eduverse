31cfe00672097f22ecaa1c7dd7cb0f0b228e3380
Git1Gud <talhanaik2004@gmail.com>
2024-10-05 12:34:19 +0530

    assignment Controller fixed

--- Backend/controllers/assignmentControllers.js
+++ Backend/controllers/assignmentControllers.js
@@ -124,7 +124,7 @@ export const gradeAssignmentController=async(req,res)=>{
         const submission=assignment.submissions.find((submission)=>submission.student==studentId)
         const pdf_url=submission.submission
         console.log({pdf_url,criteria});        
-        const response=await axios.post(`${process.env.FLASK_URL}/upload`,{pdf_url,criteria},{
+        const response=await axios.post(`${process.env.FLASK_URL}/grade`,{pdf_url,criteria},{
             headers:{
                 "Content-type":"application/json",
             },
